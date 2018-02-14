7221a70a9100163944d974ac1878d8398c5bbfaa
Simen S. Røstad <simsigro@gmail.com>
2018-02-14 19:18:11 +0100

    Deletet test

--- examples/ble_central/RELAYER test med connection/pca10040/s132/ses/Output/ble_app_uart_c_pca10040_s132 Debug/Obj/SEGGER_RTT_Syscalls_SES.d
+++
@@ -1,3 +0,0 @@
-Output/ble_app_uart_c_pca10040_s132\ Debug/Obj/SEGGER_RTT_Syscalls_SES.o: \
- /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/external/segger_rtt/SEGGER_RTT_Syscalls_SES.c \
- ../config/sdk_config.h
