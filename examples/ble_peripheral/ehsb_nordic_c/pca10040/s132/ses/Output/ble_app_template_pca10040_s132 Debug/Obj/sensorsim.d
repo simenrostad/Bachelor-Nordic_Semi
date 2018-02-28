76b4eda17216da36230b436696c0cdd660d127b2
Simen S. Røstad <simsigro@gmail.com>
2018-02-28 12:05:29 +0100

    ...

--- examples/ble_peripheral/ehsb_nordic_c/pca10040/s132/ses/Output/ble_app_template_pca10040_s132 Debug/Obj/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/ble_app_template_pca10040_s132\ Debug/Obj/sensorsim.o: \
- /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/components/libraries/sensorsim/sensorsim.c \
- /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/components/libraries/sensorsim/sensorsim.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h
