7e83b30cdc27006a6cfd367add2d0928af98400a
Simen S. Røstad <simsigro@gmail.com>
2018-03-24 21:43:47 +0100

    ...

---
+++ Software/ehsb/ehsb_nordic_r/pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Debug/Obj/app_error.d
@@ -0,0 +1,24 @@
+Output/ble_app_uart_pca10040_s132\ Debug/Obj/app_error.o: \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Software/sdk/components/libraries/util/app_error.c \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Software/sdk/components/libraries/util/app_error.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Software/sdk/components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../config/sdk_config.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Software/sdk/components/libraries/util/nordic_common.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Software/sdk/components/libraries/util/app_error_weak.h
---
+++ Software/ehsb/ehsb_nordic_r/pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Debug/Obj/ses_nrf52_Vectors.d
@@ -0,0 +1,2 @@
+Output/ble_app_uart_pca10040_s132\ Debug/Obj/ses_nrf52_Vectors.o: \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Software/components/toolchain/ses/ses_nrf52_Vectors.s
