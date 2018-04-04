e29c74f185abd4a4741b0cc480d9aa4a56074d25
innafor <innafor@users.noreply.github.com>
2018-04-04 11:18:36 +0200

    Moving the projects

--- Software/examples/ble_central/EHSB_NORDIC_C/pca10040/s132/ses/Output/ble_ehsb_c_pca10040_s132 Debug/Obj/SEGGER_RTT_Syscalls_SES.d
+++
@@ -1,3 +0,0 @@
-Output/ble_ehsb_c_pca10040_s132\ Debug/Obj/SEGGER_RTT_Syscalls_SES.o: \
- C:\Users\Torus\Documents\GitHub\Bachelor-Nordic_Semi\Software\external\segger_rtt\SEGGER_RTT_Syscalls_SES.c \
- ../config/sdk_config.h
--- Software/examples/ble_central/EHSB_NORDIC_C/pca10040/s132/ses/Output/ble_ehsb_c_pca10040_s132 Debug/Obj/ses_nrf52_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/ble_ehsb_c_pca10040_s132\ Debug/Obj/ses_nrf52_Vectors.o: \
- C:\Nordic_semi\nRF5_SDK_14.2.0_17b948a\components\toolchain\ses\ses_nrf52_Vectors.s
