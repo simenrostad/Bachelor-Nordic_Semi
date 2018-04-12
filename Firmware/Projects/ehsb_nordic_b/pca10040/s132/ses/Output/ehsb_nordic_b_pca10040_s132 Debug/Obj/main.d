75a745174dac9afba8e4b3a72a591a1c02c96ef1
innafor <innafor@users.noreply.github.com>
2018-04-12 11:52:47 +0200

    mappegibb

---
+++ Firmware/Projects/ehsb_nordic_b/pca10040/s132/ses/Output/ehsb_nordic_b_pca10040_s132 Debug/Obj/main.d
@@ -0,0 +1,66 @@
+Output/ehsb_nordic_b_pca10040_s132\ Debug/Obj/main.o: \
+ C:\GitHub\Bachelor-Nordic_Semi\Firmware\Projects\ehsb_nordic_b\main.c \
+ ../../../../../External/components/softdevice/s132/headers/nrf_soc.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../External/components/device/nrf.h \
+ ../../../../../External/components/device/nrf52.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../External/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../External/components/toolchain/system_nrf52.h \
+ ../../../../../External/components/device/nrf52_bitfields.h \
+ ../../../../../External/components/device/nrf51_to_nrf52.h \
+ ../../../../../External/components/device/nrf52_name_change.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../External/components/softdevice/common/nrf_sdh.h \
+ ../config/sdk_config.h \
+ ../../../../../External/components/libraries/util/sdk_errors.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../External/components/libraries/experimental_section_vars/nrf_section_iter.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ ../../../../../External/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../External/components/libraries/util/nordic_common.h \
+ ../../../../../External/components/libraries/util/nrf_assert.h \
+ ../../../../../External/components/libraries/util/app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
+ ../../../../../External/components/libraries/util/sdk_errors.h \
+ ../../../../../External/components/libraries/util/nordic_common.h \
+ ../../../../../External/components/libraries/util/app_error_weak.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/softdevice/common/nrf_sdh_ble.h \
+ ../../../../../External/components/softdevice/s132/headers/ble.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_ranges.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_types.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_l2cap.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_err.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gattc.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gatts.h \
+ ../../../../../External/components/ble/common/ble_advdata.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/string.h \
+ ../../../../../External/components/libraries/experimental_log/nrf_log.h \
+ ../../../../../External/components/libraries/util/sdk_common.h \
+ ../../../../../External/components/libraries/util/sdk_os.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ ../../../../../External/components/libraries/util/sdk_macros.h \
+ ../../../../../External/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../External/components/libraries/experimental_log/src/nrf_log_internal.h \
+ ../../../../../External/components/libraries/experimental_log/nrf_log_ctrl.h \
+ ../../../../../External/components/libraries/experimental_log/src/nrf_log_ctrl_internal.h \
+ ../../../../../External/components/libraries/experimental_log/nrf_log_backend_interface.h \
+ ../../../../../External/components/libraries/experimental_memobj/nrf_memobj.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdlib.h \
+ ../../../../../External/components/libraries/balloc/nrf_balloc.h \
+ ../../../../../External/components/libraries/util/app_util_platform.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_nvic.h \
+ ../../../../../External/components/libraries/util/nrf_assert.h \
+ ../../../../../External/components/libraries/experimental_log/nrf_log_default_backends.h
---
+++ Firmware/Projects/ehsb_nordic_b/pca10040/s132/ses/Output/ehsb_nordic_b_pca10040_s132 Debug/Obj/nrf_log_frontend.d
@@ -0,0 +1,28 @@
+Output/ehsb_nordic_b_pca10040_s132\ Debug/Obj/nrf_log_frontend.o: \
+ C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\libraries\experimental_log\src\nrf_log_frontend.c \
+ ../../../../../External/components/libraries/util/sdk_common.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/string.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ ../config/sdk_config.h \
+ ../../../../../External/components/libraries/util/nordic_common.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/libraries/util/sdk_os.h \
+ ../../../../../External/components/libraries/util/sdk_errors.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stddef.h \
+ ../../../../../External/components/device/nrf.h \
+ ../../../../../External/components/device/nrf52.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../External/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../External/components/toolchain/system_nrf52.h \
+ ../../../../../External/components/device/nrf52_bitfields.h \
+ ../../../../../External/components/device/nrf51_to_nrf52.h \
+ ../../../../../External/components/device/nrf52_name_change.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/libraries/util/sdk_macros.h
