8ab49e23894d9693c665762cbfd90fbcc2f387f0
Simen S. Røstad <simsigro@gmail.com>
2018-04-17 16:42:41 +0200

    .

---
+++ Firmware/Projects/ehsb_nordic_r/pca10040/s132/ses/Output/ehsb_nordic_r_pca10040_s132 Debug/Obj/app_button.d
@@ -0,0 +1,44 @@
+Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/app_button.o: \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/button/app_button.c \
+ ../../../../../External/components/libraries/util/sdk_common.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/string.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ ../config/sdk_config.h \
+ ../../../../../External/components/libraries/util/nordic_common.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/libraries/util/sdk_os.h \
+ ../../../../../External/components/libraries/util/sdk_errors.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stddef.h \
+ ../../../../../External/components/device/nrf.h \
+ ../../../../../External/components/device/nrf52.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../External/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../External/components/toolchain/system_nrf52.h \
+ ../../../../../External/components/device/nrf52_bitfields.h \
+ ../../../../../External/components/device/nrf51_to_nrf52.h \
+ ../../../../../External/components/device/nrf52_name_change.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/libraries/util/sdk_macros.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/button/app_button.h \
+ ../../../../../External/components/libraries/util/app_error.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
+ ../../../../../External/components/libraries/util/app_error_weak.h \
+ ../../../../../External/components/drivers_nrf/hal/nrf_gpio.h \
+ ../../../../../External/components/drivers_nrf/hal/nrf_peripherals.h \
+ ../../../../../External/components/device/nrf52832_peripherals.h \
+ ../../../../../External/components/libraries/util/nrf_assert.h \
+ ../../../../../External/components/libraries/util/app_error.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdlib.h \
+ ../../../../../External/components/libraries/timer/app_timer.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ ../../../../../External/components/libraries/util/nordic_common.h \
+ ../../../../../External/components/drivers_nrf/gpiote/nrf_drv_gpiote.h \
+ ../../../../../External/components/drivers_nrf/hal/nrf_gpiote.h \
+ ../../../../../External/components/libraries/util/sdk_errors.h
---
+++ Firmware/Projects/ehsb_nordic_r/pca10040/s132/ses/Output/ehsb_nordic_r_pca10040_s132 Debug/Obj/ble_advdata.d
@@ -0,0 +1,45 @@
+Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/ble_advdata.o: \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_advdata.c \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_advdata.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/string.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ ../../../../../External/components/softdevice/s132/headers/ble.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_ranges.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_types.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_l2cap.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_err.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gattc.h \
+ ../../../../../External/components/device/nrf.h \
+ ../../../../../External/components/device/nrf52.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../External/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../External/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../External/components/toolchain/system_nrf52.h \
+ ../../../../../External/components/device/nrf52_bitfields.h \
+ ../../../../../External/components/device/nrf51_to_nrf52.h \
+ ../../../../../External/components/device/nrf52_name_change.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gatts.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stddef.h \
+ ../../../../../External/components/device/compiler_abstraction.h \
+ ../../../../../External/components/libraries/util/nordic_common.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gap.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_srv_common.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_types.h \
+ ../../../../../External/components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../External/components/libraries/util/sdk_common.h \
+ ../config/sdk_config.h \
+ ../../../../../External/components/libraries/util/sdk_os.h \
+ ../../../../../External/components/libraries/util/sdk_errors.h \
+ ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../External/components/libraries/util/app_util.h \
+ ../../../../../External/components/libraries/util/sdk_macros.h
