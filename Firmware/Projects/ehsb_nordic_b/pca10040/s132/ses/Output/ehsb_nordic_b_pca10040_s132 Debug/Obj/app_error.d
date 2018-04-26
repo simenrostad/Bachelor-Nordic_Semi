6d296805eda0a8c92f8142922f2eb603b21495d0
Simen S. Røstad <simsigro@gmail.com>
2018-04-26 09:57:33 +0200

    ...

--- Firmware/Projects/ehsb_nordic_b/pca10040/s132/ses/Output/ehsb_nordic_b_pca10040_s132 Debug/Obj/app_error.d
+++ Firmware/Projects/ehsb_nordic_b/pca10040/s132/ses/Output/ehsb_nordic_b_pca10040_s132 Debug/Obj/app_error.d
@@ -1,9 +1,9 @@
 Output/ehsb_nordic_b_pca10040_s132\ Debug/Obj/app_error.o: \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\libraries\util\app_error.c \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/util/app_error.c \
  ../../../../../External/components/device/nrf.h \
  ../../../../../External/components/device/nrf52.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cmInstr.h \
  ../../../../../External/components/toolchain/cmsis/include/cmsis_gcc.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cmFunc.h \
@@ -13,12 +13,12 @@ Output/ehsb_nordic_b_pca10040_s132\ Debug/Obj/app_error.o: \
  ../../../../../External/components/device/nrf51_to_nrf52.h \
  ../../../../../External/components/device/nrf52_name_change.h \
  ../../../../../External/components/device/compiler_abstraction.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\libraries\util\app_error.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\libraries\util\sdk_errors.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/util/app_error.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/util/sdk_errors.h \
  ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
  ../config/sdk_config.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\libraries\util\nordic_common.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\libraries\util\app_error_weak.h
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/util/nordic_common.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/libraries/util/app_error_weak.h
