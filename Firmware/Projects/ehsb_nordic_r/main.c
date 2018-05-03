cf067d2678f5decb10af4915312c0166c94e1a02
innafor <innafor@users.noreply.github.com>
2018-05-03 16:05:08 +0200

    gibb

--- Firmware/Projects/ehsb_nordic_r/main.c
+++ Firmware/Projects/ehsb_nordic_r/main.c
@@ -92,23 +92,17 @@
 
 #define APP_ADV_INTERVAL                64                                          /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
 
-#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(25, UNIT_1_25_MS)             /**< Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
-#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(75, UNIT_1_25_MS)             /**< Maximum acceptable connection interval (75 ms), Connection interval uses 1.25 ms units. */
+#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)             /**< Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)             /**< Maximum acceptable connection interval (75 ms), Connection interval uses 1.25 ms units. */
 #define SLAVE_LATENCY                   0                                           /**< Slave latency. */
 #define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds), Supervision Timeout uses 10 ms units. */
 #define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(5000)                       /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000)                      /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
 
-<<<<<<< HEAD
 #define SCAN_INTERVAL           0x00A0                                              /**< Determines scan interval in units of 0.625 millisecond. */
 #define SCAN_WINDOW             0x0090                                              /**< Determines scan window in units of 0.625 millisecond. */
 #define SCAN_TIMEOUT            0x0078                                              /**< Timeout when scanning in units of seconds. 0x0000 disables timeout. */
-=======
-#define SCAN_INTERVAL           0x0070                                              /**< Determines scan interval in units of 0.625 millisecond. */
-#define SCAN_WINDOW             0x0060                                              /**< Determines scan window in units of 0.625 millisecond. */
-#define SCAN_TIMEOUT            0x0000                                              /**< Timeout when scanning. 0x0000 disables timeout. */
->>>>>>> c254d8b7436e607efb6452a6d4f0e1af15749033
 
 #define UUID128_SIZE                    16
 
