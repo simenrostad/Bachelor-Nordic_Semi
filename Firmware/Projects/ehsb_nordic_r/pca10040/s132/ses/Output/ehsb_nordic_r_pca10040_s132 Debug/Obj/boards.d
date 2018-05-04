6dda258c5c5cf889c264c0721e684564af5ad814
innafor <innafor@users.noreply.github.com>
2018-05-04 11:52:29 +0200

    ymse

--- Firmware/Projects/ehsb_nordic_r/pca10040/s132/ses/Output/ehsb_nordic_r_pca10040_s132 Debug/Obj/boards.d
+++ Firmware/Projects/ehsb_nordic_r/pca10040/s132/ses/Output/ehsb_nordic_r_pca10040_s132 Debug/Obj/boards.d
@@ -1,11 +1,11 @@
 Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/boards.o: \
- /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/boards/boards.c \
- /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/boards/boards.h \
+ C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\boards\boards.c \
+ C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\boards\boards.h \
  ../../../../../External/components/drivers_nrf/hal/nrf_gpio.h \
  ../../../../../External/components/device/nrf.h \
  ../../../../../External/components/device/nrf52.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cmInstr.h \
  ../../../../../External/components/toolchain/cmsis/include/cmsis_gcc.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cmFunc.h \
@@ -19,13 +19,13 @@ Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/boards.o: \
  ../../../../../External/components/device/nrf52832_peripherals.h \
  ../../../../../External/components/libraries/util/nrf_assert.h \
  ../../../../../External/components/libraries/util/app_error.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
  ../../../../../External/components/libraries/util/sdk_errors.h \
  ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
  ../config/sdk_config.h \
  ../../../../../External/components/libraries/util/nordic_common.h \
  ../../../../../External/components/libraries/util/app_error_weak.h \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdlib.h \
- /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/boards/pca10040.h
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdlib.h \
+ C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\boards\pca10040.h
