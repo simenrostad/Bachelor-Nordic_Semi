132dc7d653839118c4594f34119bc963f83e315b
Simen S. Røstad <simsigro@gmail.com>
2018-05-16 12:03:48 +0200

    ...

--- Firmware/Projects/ehsb_nordic_r/pca10040/s132/ses/Output/ehsb_nordic_r_pca10040_s132 Debug/Obj/ble_conn_params.d
+++ Firmware/Projects/ehsb_nordic_r/pca10040/s132/ses/Output/ehsb_nordic_r_pca10040_s132 Debug/Obj/ble_conn_params.d
@@ -1,10 +1,10 @@
 Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/ble_conn_params.o: \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\ble\common\ble_conn_params.c \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_conn_params.c \
  ../../../../../External/components/libraries/util/sdk_common.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/string.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdbool.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/string.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/__crossworks.h \
  ../config/sdk_config.h \
  ../../../../../External/components/libraries/util/nordic_common.h \
  ../../../../../External/components/device/compiler_abstraction.h \
@@ -12,7 +12,7 @@ Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/ble_conn_params.o: \
  ../../../../../External/components/libraries/util/sdk_errors.h \
  ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
  ../../../../../External/components/libraries/util/app_util.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stddef.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stddef.h \
  ../../../../../External/components/device/nrf.h \
  ../../../../../External/components/device/nrf52.h \
  ../../../../../External/components/toolchain/cmsis/include/core_cm4.h \
@@ -26,12 +26,12 @@ Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/ble_conn_params.o: \
  ../../../../../External/components/device/nrf52_name_change.h \
  ../../../../../External/components/device/compiler_abstraction.h \
  ../../../../../External/components/libraries/util/sdk_macros.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdlib.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdlib.h \
  ../../../../../External/components/libraries/util/sdk_errors.h \
  ../../../../../External/components/softdevice/s132/headers/ble_hci.h \
  ../../../../../External/components/softdevice/s132/headers/ble_err.h \
  ../../../../../External/components/softdevice/s132/headers/nrf_error.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\ble\common\ble_conn_params.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_conn_params.h \
  ../../../../../External/components/softdevice/s132/headers/ble.h \
  ../../../../../External/components/softdevice/s132/headers/ble_ranges.h \
  ../../../../../External/components/softdevice/s132/headers/ble_types.h \
@@ -42,12 +42,12 @@ Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/ble_conn_params.o: \
  ../../../../../External/components/softdevice/s132/headers/ble_gatt.h \
  ../../../../../External/components/softdevice/s132/headers/ble_gattc.h \
  ../../../../../External/components/softdevice/s132/headers/ble_gatts.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\ble\common\ble_srv_common.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_srv_common.h \
  ../../../../../External/components/softdevice/s132/headers/ble_types.h \
  ../../../../../External/components/libraries/util/app_util.h \
  ../../../../../External/components/softdevice/s132/headers/ble_gap.h \
  ../../../../../External/components/softdevice/s132/headers/ble_gatt.h \
- C:\GitHub\Bachelor-Nordic_Semi\Firmware\External\components\ble\common\ble_conn_state.h \
+ /Users/simenrostad/Documents/GitHub/Bachelor-Nordic_Semi/Firmware/External/components/ble/common/ble_conn_state.h \
  ../../../../../External/components/libraries/util/sdk_mapped_flags.h \
  ../../../../../External/components/softdevice/common/nrf_sdh_ble.h \
  ../../../../../External/components/libraries/experimental_section_vars/nrf_section_iter.h \
@@ -55,7 +55,7 @@ Output/ehsb_nordic_r_pca10040_s132\ Debug/Obj/ble_conn_params.o: \
  ../../../../../External/components/libraries/util/nordic_common.h \
  ../../../../../External/components/libraries/util/nrf_assert.h \
  ../../../../../External/components/libraries/util/app_error.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.34/include/stdio.h \
  ../../../../../External/components/libraries/util/app_error_weak.h \
  ../../../../../External/components/libraries/timer/app_timer.h \
  ../../../../../External/components/libraries/util/app_error.h
